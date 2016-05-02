a8250bc41f3dd07a00782f2561c9f6214108cac7
liume <2111904043@qq.com>
2016-05-02 14:18:59 +0800

    '第三次修改'

---
+++ Second.java
@@ -0,0 +1,9 @@
+public class Second
+{   public static void showHello(String name){
+         System.out.println("hello"+name);
+    
+    } 
+    public static void main(String[] name){
+        showHello("ÀîËÄ");
+    }
+}
